93204510b76a84cbbe5d03d697a0deb307ba8351
Giolvani <inavloig@gmail.com>
2024-11-28 00:52:30 -0300

    refactor backup codebase

--- scripts/backup_codebase.py
+++ scripts/backup_codebase.py
@@ -4,63 +4,56 @@ import subprocess
 import json
 import boto3
 import shutil
-from datetime import datetime
 from logger import log_message
 
-with open("config/settings.json") as config_file:
-    config = json.load(config_file)
 
-TEMP_DIR = "./backups/codebase"
-BUCKET_NAME = config["storage"]["bucket_name"]
-ENDPOINT_URL = config["storage"]["endpoint_url"]
-ACCESS_KEY = config["storage"]["access_key"]
-SECRET_KEY = config["storage"]["secret_key"]
-
-
-s3_client = boto3.client(
-    "s3",
-    endpoint_url=ENDPOINT_URL,
-    aws_access_key_id=ACCESS_KEY,
-    aws_secret_access_key=SECRET_KEY,
-)
-
-
-def backup_repo(repo_url, profile_name, timestamp):
-    repo_name = os.path.basename(repo_url).replace(".git", "")
-    log_message(f"Iniciando backup do repositório {repo_name}.")
-
-    repo_path = os.path.join(TEMP_DIR, repo_name)
-    os.makedirs(TEMP_DIR, exist_ok=True)
-    subprocess.run(["git", "clone", "--mirror", repo_url, repo_path], check=True)
-
-    tar_path = f"{TEMP_DIR}/{repo_name}_backup.tar.gz"
-    with tarfile.open(tar_path, "w:gz") as tar:
-        tar.add(repo_path, arcname=repo_name)
-
-    storage_path = f"{timestamp}/codebase/{profile_name}/{repo_name}_backup.tar.gz"
-    log_message(f"Uploading to path: {storage_path}")
-
-    s3_client.upload_file(Bucket=BUCKET_NAME, Key=storage_path, Filename=tar_path)
-    log_message(
-        f"Backup do repositório {repo_name} do perfil {profile_name} concluído."
+def backup_repo(repo_url, repo_name, storage_config, temp_dir, timestamp):
+    s3_client = boto3.client(
+        "s3",
+        endpoint_url=storage_config["endpoint_url"],
+        aws_access_key_id=storage_config["access_key"],
+        aws_secret_access_key=storage_config["secret_key"],
     )
 
-    shutil.rmtree(TEMP_DIR)
+    log_message(f"Starting backup of repository {repo_name}.")
+    repo_path = os.path.join(temp_dir, repo_name)
+    os.makedirs(temp_dir, exist_ok=True)
+
+    try:
+        subprocess.run(["git", "clone", repo_url, repo_path], check=True)
+
+        tar_path = os.path.join(temp_dir, f"{repo_name}_backup.tar.gz")
+        with tarfile.open(tar_path, "w:gz") as tar:
+            tar.add(repo_path, arcname=repo_name)
+
+        storage_path = f"{timestamp}/codebase/{repo_name}_backup.tar.gz"
+        log_message(f"Uploading to path: {storage_path}")
+
+        s3_client.upload_file(Bucket=storage_config["bucket_name"], Key=storage_path, Filename=tar_path)
+        log_message(f"Backup of repository {repo_name} completed.")
+
+    except subprocess.CalledProcessError as e:
+        log_message(f"Error during backup of repository {repo_name}: {e}")
+    except Exception as e:
+        log_message(f"General error in repository backup {repo_name}: {e}")
+    finally:
+        shutil.rmtree(repo_path, ignore_errors=True)
 
 
-def backup_codebases(timestamp):
-    if not config["repos"]:
-        log_message("Nenhum repositório configurado para backup.")
+def backup_codebases(config_path, timestamp):
+    with open(config_path) as config_file:
+        config = json.load(config_file)
+
+    if not config.get("codebase"):
+        log_message("No repositories configured for backup.")
         return
 
-    for profile in config["repos"]:
-        log_message(
-            f"Iniciando backup dos repositórios do perfil: {profile['profile']}"
-        )
-        for repo_url in profile["repositories"]:
-            backup_repo(repo_url, profile["profile"], timestamp)
+    for group in config["codebase"]:
+        repo_name = group["repo_name"]
+        for repo_url in group["repositories"]:
+            backup_repo(repo_url, repo_name, config["storage"], "./backups/codebase", timestamp)
 
 
 if __name__ == "__main__":
-    timestamp = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
-    backup_codebases(timestamp)
+    print("You shouldn't run this file directly.")
+    pass
